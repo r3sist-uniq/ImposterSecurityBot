2fd68265a928b58b7dae5e233c477299f82a170d
R3sist <amanmatreja@gmail.com>
2023-05-06 19:13:59 +0530

    done bot

---
+++ bot.py
@@ -0,0 +1,69 @@
+import discord
+from discord.ext import commands, tasks
+import re, os
+from dotenv import load_dotenv
+
+load_dotenv()
+bot_token = os.environ['bot_token']
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+
+    print(f'Bot connected as {bot.user}')
+    check_impersonators.start()
+
+@tasks.loop(seconds=10)
+async def check_impersonators():
+    try:
+        for guild in bot.guilds:
+            owner = guild.owner
+            owner_name = owner.display_name.lower()
+            owner_nick = owner.name.lower()
+            
+            owner_name_format = r"{}".format(".*".join(list(owner_name)))
+            owner_nick_format = r"{}".format(".*".join(list(owner_nick)))
+
+            owner_name_regex = re.compile(owner_name_format, re.IGNORECASE)
+            owner_nick_regex = re.compile(owner_nick_format, re.IGNORECASE)
+
+            print(owner_name_regex)
+
+            for member in guild.members:
+                member_name = member.name.lower()
+                member_nick = member.display_name.lower()
+
+                print(member_name, member_nick)
+
+                if member != owner and (str(member) == str(owner) or
+                                        owner_name_regex.search(member_name) or
+                                        owner_nick_regex.search(member_name) or
+                                        owner_name_regex.search(member_nick) or
+                                        owner_nick_regex.search(member_nick)):
+
+                    message = f"Impersonator kicked:\nUsername: {member}\nTag: {member.discriminator}\n Nickname: {member.display_name}"
+                    await member.kick(reason='Impersonating the server owner')
+                    
+                    channel_name = "impersonation-alerts"
+
+                    channel = discord.utils.get(guild.channels, name=channel_name)
+                    if not channel:
+                        overwrites = {
+                            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                            guild.me: discord.PermissionOverwrite(read_messages=True),
+                            guild.owner: discord.PermissionOverwrite(read_messages=True),
+                            guild.roles[0]: discord.PermissionOverwrite(read_messages=False)  # Set the @everyone role to not see the channel
+                        }          
+                        new_channel = await guild.create_text_channel(channel_name, overwrites=overwrites)
+                        await new_channel.send(message)
+                    else:
+                        await channel.send(message)
+    except Exception as e:
+        print('some error happened', e)
+              
+bot.run(bot_token)
